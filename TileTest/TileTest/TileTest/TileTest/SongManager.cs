fd29db57e69be78c0117331fa46c917344db33fd
humenspider7 <iamyusuf6@aol.com>
2017-05-29 20:32:14 -0400

    Initialized Song/Sound Manager
    Song Manager is for SONGS ONLY
    
    Sound Manager is for SOUND EFFECTS ONLY

---
+++ TileTest/TileTest/TileTest/TileTest/SongManager.cs
@@ -0,0 +1,152 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Audio;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.GamerServices;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Media;
+using System.Diagnostics;
+
+namespace TileTest
+{
+    class SongManager
+    {
+        //initialize sound files here.
+        private static Song maze;
+        private static Song desert;
+        private static Song water;
+        private static Song hell;
+        private static Song spawnroom;
+
+
+
+        public static void Initialize(ContentManager content)
+        {
+            try
+            {
+                // load song files here.
+                spawnroom = content.Load<Song>(@"Sounds\overworld");
+
+                //maze = content.Load<Song>(@"Sounds\");
+                desert = content.Load<Song>(@"Sounds\sandsea");
+                water = content.Load<Song>(@"Sounds\waterTheme");
+                hell = content.Load<Song>(@"Sounds\hellTheme");
+            }
+            catch
+            {
+                Debug.Write("SoundManager Initialization Failed");
+            }
+        }
+
+        public static void playSpawnRoom()
+        {
+            try
+            {
+                MediaPlayer.Play(spawnroom);
+            }
+
+            catch
+            {
+                Debug.Write("spawnroom failed");
+            }
+        }
+
+        public static void playMaze()
+        {
+            try
+            {
+                MediaPlayer.Play(maze);
+            }
+
+            catch
+            {
+                Debug.Write("maze failed");
+            }
+        }
+
+        public static void playDesert()
+        {
+            try
+            {
+                MediaPlayer.Play(desert);
+            }
+
+            catch
+            {
+                Debug.Write("desert failed");
+            }
+        }
+
+        public static void playWater()
+        {
+            try
+            {
+                MediaPlayer.Play(water);
+            }
+
+            catch
+            {
+                Debug.Write("water failed");
+            }
+        }
+
+        public static void playHell()
+        {
+            try
+            {
+                MediaPlayer.Play(hell);
+            }
+
+            catch
+            {
+                Debug.Write("hell failed");
+            }
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
